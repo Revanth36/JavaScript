0d16da3841d1471c891b9408f28f63d0e5b5dcbc
Revanth36 <revanthreddyn1234@gmail.com>
2020-11-09 10:12:33 +0530

    manipulating complex objects

---
+++ lesson90.js
@@ -0,0 +1,25 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Billy Joel1",
+    "title": "Piano Man1",
+    "release_year": 19713,
+    "formats": [
+      "CD1",
+      "8T1",
+      "LP1"
+    ],
+    "gold": true
+  }
+  // Add a record here
+];
